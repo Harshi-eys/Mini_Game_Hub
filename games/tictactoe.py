ca3ffd1dd0c64737189a6811f423a7319e0cfacf
Kanak <25b0962@iitb.ac.in>
2026-04-01 23:37:31 +0530

    Added function to tictactoe.py

--- games/tictactoe.py
+++ games/tictactoe.py
@@ -12,11 +12,28 @@ for i in range(3):
     for j in range(3):
         board.append(pygame.Rect(41 + 109*j, 41 + 109*i, 100, 100))
 
+def turn(board, p):
+    global player
+    for i in range(9):
+        if board[i].collidepoint(pygame.mouse.get_pos()):
+            if player == sys.argv[1]:
+                pad = 12.5
+                pygame.draw.line(screen, 'White', (board[i].left + pad, board[i].top + pad), (board[i].right - pad, board[i].bottom - pad), 5)
+                pygame.draw.line(screen, 'White', (board[i].left + pad, board[i].bottom - pad), (board[i].right - pad, board[i].top + pad), 5)
+                player = sys.argv[2]
+
+            else :
+                pygame.draw.circle(screen, 'white', board[i].center, 37.5, 5)
+                player = sys.argv[1]
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            turn(board,player)
 
     pygame.draw.line(screen, 'Red', (145.5, 41), (145.5, 359), 9)
     pygame.draw.line(screen, 'Red', (254.5, 41), (254.5, 359), 9)
@@ -25,6 +42,3 @@ while True:
 
     
     pygame.display.update()
-
-
-
