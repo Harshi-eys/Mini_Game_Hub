f5c3d1eb0d9475b98d23b38d8354b75f00288e6b
Kanak <25b0962@iitb.ac.in>
2026-04-08 23:55:08 +0530

    Animation and function turn added

--- games/connect4.py
+++ games/connect4.py
@@ -24,7 +24,49 @@ for i in range(7):
 bgi1 = pygame.image.load("./Ref_Images/C4_bg_cutout.png").convert_alpha()
 bgi1 = pygame.transform.scale(bgi1, screen.get_size())
 
+x = yf = 0
+y = -55
+coin = None
+falling = False
+
+def turn(board):
+    global player, x, y, yf, coin, falling
+
+    if falling:
+        return
+
+    for i in range(7):
+        if board[i]['rect'].collidepoint(pygame.mouse.get_pos()):
+            for j in range(6, -1, -1):
+                x = 156 + 76*i
+                y = 0
+                yf = 162 + 65*j
+                falling = True
+
+                if board[i]['cell'][j] == 0:
+                    if player == sys.argv[1]:
+                        player = sys.argv[2]
+                        board[i]['cell'][j] = 1
+                        coin = p1
+                        break
+
+                    else:
+                        player = sys.argv[1]
+                        board[i]['cell'][j] = 1
+                        coin = p2
+                        break
+
+            return
+
+clock = pygame.time.Clock()
+
 while True:
+
+    if falling:
+        y +=10
+        if y >= yf:
+            y = yf
+            falling = False
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -32,8 +74,12 @@ while True:
             sys.exit()
 
         if event.type == pygame.MOUSEBUTTONDOWN:
-            print(pygame.mouse.get_pos())
+            #print(pygame.mouse.get_pos())
+            turn(board)
 
     screen.blit(bgi, (0,0))
+    if falling:
+        screen.blit(coin, (x, y))
     screen.blit(bgi1, (0,0))
     pygame.display.update()
+    clock.tick(60)
