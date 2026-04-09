7a695b9a713e2e8c395de105bec59a40726a5246
Harshi <harshi@Harshis-MacBook-Air.local>
2026-04-09 14:42:13 +0530

    Used numpy and class in tictactoe and connect4

--- games/connect4.py
+++ games/connect4.py
@@ -1,10 +1,10 @@
 import pygame
 import sys
+import numpy as np
 
 pygame.init()
 screen = pygame.display.set_mode((800,800))
 pygame.display.set_caption("Connect4")
-player = sys.argv[1]
 
 p1 = pygame.image.load("./Ref_Images/C4_Red.png").convert_alpha()
 p1 = pygame.transform.scale(p1, (51, 55))
@@ -13,60 +13,88 @@ p2 = pygame.transform.scale(p2, (51, 55))
 
 bgi = pygame.image.load("./Ref_Images/C4_bg.png").convert()
 bgi = pygame.transform.scale(bgi, screen.get_size())
-screen.blit(bgi, (0,0))
 
-board = {}
+rect = []
 for i in range(7):
-    rect = pygame.Rect(156 + 76*i, 155, 52, 440)
-    board[i] = {'rect':rect, 'cell':[0, 0, 0, 0, 0, 0, 0]}
-#    pygame.draw.rect(screen, 'white', board[i])
+    rect.append(pygame.Rect(156 + 76*i, 155, 52, 440))
 
 bgi1 = pygame.image.load("./Ref_Images/C4_bg_cutout.png").convert_alpha()
 bgi1 = pygame.transform.scale(bgi1, screen.get_size())
 
-x = yf = 0
-y = -55
-coin = None
-falling = False
+class Game :
+    def __init__(self):
+        self.board = np.zeros((7,7), dtype = int)
+        self.player = 1
+        self.x = self.yf = 0
+        self.y = -55
+        self.coin = None
+        self.falling = False
+        self.turn_i = None
+        self.turn_j = None
+        self.pend_player = None
 
-def turn(board):
-    global player, x, y, yf, coin, falling
-
-    if falling:
-        return
-
-    for i in range(7):
-        if board[i]['rect'].collidepoint(pygame.mouse.get_pos()):
-            for j in range(6, -1, -1):
-                x = 156 + 76*i
-                y = 0
-                yf = 162 + 65*j
-                falling = True
-
-                if board[i]['cell'][j] == 0:
-                    if player == sys.argv[1]:
-                        player = sys.argv[2]
-                        board[i]['cell'][j] = 1
-                        coin = p1
-                        break
-
-                    else:
-                        player = sys.argv[1]
-                        board[i]['cell'][j] = 1
-                        coin = p2
-                        break
-
+    def turn(self):
+        if self.falling:
             return
+        for i in range(7):
+            if rect[i].collidepoint(pygame.mouse.get_pos()):
+                for j in range(6, -1, -1):
+                    if self.board[i][j] == 0:
+                        self.x = 156 + 75.5*i
+                        self.y = -55
+                        self.yf = 158 + 64*j
+                        self.falling = True
+                        self.turn_i = i
+                        self.turn_j = j
 
+                        if self.player == 1:
+                            self.pend_player = 1
+                            self.player = 2
+                            self.coin = p1
+                            break
+
+                        else:
+                            self.pend_player = 2
+                            self.player = 1
+                            self.coin = p2
+                            break
+                return
+        
+    def win(self):
+        for i in range(7):
+            for j in range(4):
+                if self.board[i][j] == self.board[i][j+1] and self.board[i][j] == self.board[i][j+2] and self.board[i][j] == self.board[i][j+3] and self.board[i][j] != 0:
+                    return self.board[i][j]
+
+        for i in range(4):
+            for j in range(7):
+                if self.board[i][j] == self.board[i+1][j] and self.board[i][j] == self.board[i+2][j] and self.board[i][j] == self.board[i+3][j] and self.board[i][j] != 0:
+                    return self.board[i][j]
+
+        for i in range(0, 4, 1):
+            for j in range(3, 7, 1):
+                if self.board[i][j] == self.board[i+1][j-1] and self.board[i][j] == self.board[i+2][j-2] and self.board[i][j] == self.board[i+3][j-3] and self.board[i][j] != 0:
+                    return self.board[i][j]
+                
+        for i in range(0, 4, 1):
+            for j in range(0, 4, 1):
+                if self.board[i][j] == self.board[i+1][j+1] and self.board[i][j] == self.board[i+2][j+2] and self.board[i][j] == self.board[i+3][j+3] and self.board[i][j] != 0:
+                    return self.board[i][j]
+        return 0
+
+game = Game()
 clock = pygame.time.Clock()
 
 while True:
 
-    if falling:
-        y +=10
-        if y >= yf:
-            y = yf
-            falling = False
+    if game.falling:
+        game.y += 7.5
+        if game.y >= game.yf:
+            game.y = game.yf
+            game.falling = False
+            game.board[game.turn_i][game.turn_j] = game.pend_player
+            game.turn_i = None
+            game.turn_j = None
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -74,12 +102,21 @@ while True:
             sys.exit()
 
         if event.type == pygame.MOUSEBUTTONDOWN:
-            #print(pygame.mouse.get_pos())
-            turn(board)
+            if game.win() == 0 :
+                game.turn()
 
     screen.blit(bgi, (0,0))
-    if falling:
-        screen.blit(coin, (x, y))
+
+    if game.falling:
+        screen.blit(game.coin, (game.x, game.y))
+
+    for i in range(7):
+        for j in range(7):
+            if game.board[i][j] == 1 :
+                screen.blit(p1, (156 + 75.5*i, 158 + 64*j))
+            elif game.board[i][j] == 2 :
+                screen.blit(p2, (156 + 75.5*i, 158 + 64*j))
+
     screen.blit(bgi1, (0,0))
     pygame.display.update()
     clock.tick(60)
--- games/tictactoe.py
+++ games/tictactoe.py
@@ -1,77 +1,81 @@
 import pygame
 import sys 
+import numpy as np
 
 pygame.init()
 screen = pygame.display.set_mode((400,400))
-pygame.display.set_caption("Game")
-board=[]
-player = sys.argv[1]
+pygame.display.set_caption("Tic Tac Toe")
 
-pygame.draw.line(screen, 'Red', (145.5, 41), (145.5, 359), 9)
-pygame.draw.line(screen, 'Red', (254.5, 41), (254.5, 359), 9)
-pygame.draw.line(screen, 'Red', (41, 145.5), (359, 145.5), 9)
-pygame.draw.line(screen, 'Red', (41, 254.5), (359, 254.5), 9)
-
+rect = []
 for i in range(3):
     for j in range(3):
-        board.append(pygame.Rect(41 + 109*j, 41 + 109*i, 100, 100))
+        rect.append(pygame.Rect(41 + 109*j, 41 + 109*i, 100, 100))
 
-value = []
-for i in range(9):
-    value.append(0)
+class Game :
+    def __init__(self):
+        self.board = np.zeros((3,3), dtype = int)
+        self.player = 1
 
-def turn(board):
-    global player
-    for i in range(9):
-        if board[i].collidepoint(pygame.mouse.get_pos()):
-            if value[i] != 0:
-                return
+    def turn(self):
+        for i in range(9):
+            if rect[i].collidepoint(pygame.mouse.get_pos()):
+                if self.board[i//3][i % 3] != 0:
+                    return
+                
+                if self.player == 1:
+                    self.player = 2
+                    self.board[i//3][i%3] = 1
+
+                else :
+                    self.player = 1
+                    self.board[i//3][i%3] = 2
+
+    def win(self):
+        for i in range(3):
+            if self.board[0][i] == self.board[1][i] and self.board[0][i] == self.board[2][i] and self.board[0][i] != 0:
+                pygame.draw.line(screen, 'orange', (rect[i].centerx, rect[i].centery - 40), (rect[i+6].centerx, rect[i+6].centery + 40), 4)
+                return self.board[0][i]
             
-            if player == sys.argv[1]:
-                pad = 12.5
-                pygame.draw.line(screen, 'White', (board[i].left + pad, board[i].top + pad), (board[i].right - pad, board[i].bottom - pad), 5)
-                pygame.draw.line(screen, 'White', (board[i].left + pad, board[i].bottom - pad), (board[i].right - pad, board[i].top + pad), 5)
-                player = sys.argv[2]
-                value[i] = 'X'
-                win(board)
-
-            else :
-                pygame.draw.circle(screen, 'white', board[i].center, 37.5, 5)
-                player = sys.argv[1]
-                value[i] = 'O'
-                win(board)
-
-def win(board):
-
-    for i in range(3):
-        if value[i] == value[i+3] and value[i] == value[i+6] and value[i] != 0:
-            pygame.draw.line(screen, 'orange', (board[i].centerx, board[i].centery - 40), (board[i+6].centerx, board[i+6].centery + 40), 4)
-            return 1
+        for i in range(3):
+            if self.board[i][0] == self.board[i][1] and self.board[i][0] == self.board[i][2] and self.board[i][0] != 0:
+                pygame.draw.line(screen, 'orange', (rect[i].centerx - 40, rect[i].centery ), (rect[i+2].centerx + 40, rect[i+2].centery), 4)
+                return self.board[i][0]
+            
+        if self.board[0][0] == self.board[1][1] and self.board[0][0] == self.board[2][2] and self.board[0][0] != 0:
+            pygame.draw.line(screen, 'orange', (rect[0].centerx - 40, rect[0].centery - 40), (rect[8].centerx + 40, rect[8].centery + 40), 4)
+            return self.board[0][0]
         
-    for i in {0, 3, 6}:
-        if value[i] == value[i+1] and value[i] == value[i+2] and value[i] != 0:
-            pygame.draw.line(screen, 'orange', (board[i].centerx - 40, board[i].centery ), (board[i+2].centerx + 40, board[i+2].centery), 4)
-            return 1
-        
-    if value[0] == value[4] and value[0] == value[8] and value[0] != 0:
-        pygame.draw.line(screen, 'orange', (board[0].centerx - 40, board[0].centery - 40), (board[8].centerx + 40, board[8].centery + 40), 4)
-        return 1
-    
-    elif value[2] == value[4] and value[2] == value[6] and value[2] != 0:
-        pygame.draw.line(screen, 'orange', (board[2].centerx + 40, board[2].centery - 40), (board[6].centerx - 40, board[6].centery + 40), 4)
-        return 1
-    
-    return 0
+        elif self.board[0][2] == self.board[1][1] and self.board[0][2] == self.board[2][0] and self.board[0][2] != 0:
+            pygame.draw.line(screen, 'orange', (rect[2].centerx + 40, rect[2].centery - 40), (rect[6].centerx - 40, rect[6].centery + 40), 4)
+            return self.board[0][2]    
+            
+        return 0
 
-    
+game = Game()
+
 while True:
+    screen.fill('black')
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
 
         if event.type == pygame.MOUSEBUTTONDOWN:
-            if not win(board):
-                turn(board)
+            if game.win() == 0:
+                game.turn()
 
+    winner = game.win()
+
+    for i in range(9):
+        if game.board[i//3][i%3] == 1:
+            pad = 12.5
+            pygame.draw.line(screen, 'White', (rect[i].left + pad, rect[i].top + pad), (rect[i].right - pad, rect[i].bottom - pad), 5)
+            pygame.draw.line(screen, 'White', (rect[i].left + pad, rect[i].bottom - pad), (rect[i].right - pad, rect[i].top + pad), 5)
+        elif game.board[i//3][i%3] == 2:
+            pygame.draw.circle(screen, 'white', rect[i].center, 37.5, 5)
+
+    pygame.draw.line(screen, 'Red', (145.5, 41), (145.5, 359), 9)
+    pygame.draw.line(screen, 'Red', (254.5, 41), (254.5, 359), 9)
+    pygame.draw.line(screen, 'Red', (41, 145.5), (359, 145.5), 9)
+    pygame.draw.line(screen, 'Red', (41, 254.5), (359, 254.5), 9)
     pygame.display.update()
