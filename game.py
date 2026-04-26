87a7adf9aae9139f1ba81cfe5a70a936f9a41b59
Kanak <25b0962@iitb.ac.in>
2026-04-26 16:18:37 +0530

    Added audio and removed video

--- game.py
+++ game.py
@@ -1,61 +1,32 @@
 import pygame
-from pyvidplayer2 import Video
 import sys
 import os
+from pathlib import Path
+import pygame
 
-# --- 1. Initialization ---
 pygame.init()
-SIZE = 800
-screen = pygame.display.set_mode((SIZE, SIZE))
+pygame.mixer.init()
+size = 800
+screen = pygame.display.set_mode((size, size))
 pygame.display.set_caption("Mini Game Hub")
 clock = pygame.time.Clock()
 
-# --- 2. Load Assets (Updated Paths) ---
+Mini = Path(__file__).parent.parent
+pygame.mixer.music.load(str(Mini / "Mini_Game_Hub/Ref_Audios/bg_music.mp3"))
+pygame.mixer.music.play(-1) 
 
-# Path to Video
-video_path = os.path.join("Ref_Videos", "menu2.mp4")
-intro_video = Video(video_path)
-intro_video.set_volume(50) 
+menu = pygame.image.load(str(Mini / "Mini_Game_Hub/Ref_Images/game_bg.png")).convert()
+menu = pygame.transform.scale(menu, screen.get_size())
 
-# Path to Image
-image_path = os.path.join("Ref_Images", "menu_bg.png")
-menu_img = pygame.image.load(image_path)
-menu_img = pygame.transform.scale(menu_img, (SIZE, SIZE))
-
-# Path to Audio
-audio_path = os.path.join("Ref_Audios", "bg_music.mp3")
-pygame.mixer.music.load(audio_path)
-pygame.mixer.music.play(-1)
-
-# --- 3. The Main Loop ---
-is_intro = True
 running = True
 
 while running:
-    mouse_pos = pygame.mouse.get_pos()
-    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-            
-        if event.type == pygame.MOUSEBUTTONDOWN and not is_intro:
-            # Check coordinates for your 3 games here
-            x, y = mouse_pos
-            if 250 < x < 550:
-                if 280 < y < 360:
-                    print("Launching Tic-Tac-Toe...")
-                elif 370 < y < 450:
-                    print("Launching Othello...")
-                elif 460 < y < 540:
-                    print("Launching Connect4...")
+        else : continue
 
-    if is_intro:
-        intro_video.draw(screen, (0, 0))
-        if not intro_video.active:
-            is_intro = False
-            intro_video.close()
-    else:
-        screen.blit(menu_img, (0, 0))
+    screen.blit(menu, (0, 0))
 
     pygame.display.update()
     clock.tick(60)
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+while running:
+#     mouse_pos = pygame.mouse.get_pos()
+    
+#     for event in pygame.event.get():
+#         if event.type == pygame.QUIT:
+#             running = False
+#         else : continue
