5ec5582375016786fec5cf6b8e16c8b55f8942a7
Harshi <harshi@Harshis-MacBook-Air.local>
2026-04-26 18:38:22 +0530

    Deleted unnecessary files

--- tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-while running:
-#     mouse_pos = pygame.mouse.get_pos()
-    
-#     for event in pygame.event.get():
-#         if event.type == pygame.QUIT:
-#             running = False
-#         else : continue
